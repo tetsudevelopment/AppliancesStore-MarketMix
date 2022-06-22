375a0924382e7fc73b7a59c5943034c71d9be14d
tetsudevelopment <bresmacawork@gmail.com>
2022-06-22 16:13:41 -0500

    Creando la base de datos

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 let app = new Vue({
   el: "#app",
   data: {
-    data
-
-
-    length:0,
+    dataAppliances: [
+      {
+        category: "Tv",
+        id: "tv01",
+        serial: 001,
+        name: 'Televisor LG 50" 4K UHD Smart TV 50UP7500PSF',
+        img: "assets/electrodomesticos/tv1.webp",
+        description:
+          "Pantalla LED diseño que aumenta experiencia visual * Active HDR para detalles realistas * Sonido Ultra Surround mejora la experiencia auditiva * Smart TV la manera fácil de disfrutar sin teclear al contenido favorito",
+        price: 1999900,
+        cant: 0,
+      },
+    ],
+    length: 0,
   },
-  computed: {
-  },
-  methods: {
-    
-  },
+  computed: {},
+  methods: {},
 });
